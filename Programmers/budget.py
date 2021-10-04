38e0171042c80d489ec7fe447de20e2461192b78
Jake <rancho974@gmail.com>
2021-10-04 23:46:09 +0900

    budget update to be stacked

--- Programmers/budget.py
+++ Programmers/budget.py
@@ -1,16 +1,14 @@
 def solution(d, budget):
-    begin = 0
-    end = len(d) - 1
-    d.sort()
-    while (begin == end):
-        mid = (begin + end) // 2
-        if budget == sum(d[:mid+1]):
-            return end
-        elif (begin < end):
-            end = mid - 1
-        else:
-            begin = mid + 1
-    return begin
+	answer = 0    
+	d.sort()
+	while budget > 0:
+		if budget - d[0] >= 0:
+			budget -= d.pop(0)
+			answer += 1
+		else:
+			return answer
+	return answer
+
 '''
 주어진 예산에 딱 맞으면서
 최대한 많은 물품을 살때의
@@ -18,4 +16,5 @@ def solution(d, budget):
 
 파라메트릭 서치인줄 알았으나
 백트레킹하며 완전탐색을 하여야 가능할것이라 생각든다.
+stack으로 푸는 방법
 '''
