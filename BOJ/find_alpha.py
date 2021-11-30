9f98078b612195cf5f7933dc96024af359cf851c
Jinho <rancho974@gmail.com>
2021-12-01 02:10:28 +0900

    find alpha 10809

---
+++ BOJ/find_alpha.py
@@ -0,0 +1,8 @@
+alpha_list = [ -1 for _ in range(ord('a'), ord('z')+1)]
+for idx, ch in enumerate(list(input())):
+	al_ord = ord(ch) - 97
+	if alpha_list[al_ord] == -1:
+		alpha_list[al_ord] = idx
+
+for ch in alpha_list:
+	print(ch, end=' ')
