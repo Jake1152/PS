6983a47bd6a0849f173e876cff3b479eb7fbb62c
Jake1152 <rancho974@gmail.com>
2022-02-14 12:01:49 +0900

    union-find, dfs 2가지 방법

--- BOJ/connected_component_cnt_11724.py
+++ BOJ/connected_component_cnt_11724.py
@@ -1,6 +1,42 @@
 # connected element count 11724
+import sys
+from collections import defaultdict
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+
+#2. way, dfs
+'''
+1. 인접노드들을 다 연결한다.
+2. 모든 정점을 방문할때까지 dfs를 한다.
+'''
+def dfs(node):
+	visited[node] = True
+	# print(f"{node=} in dfs")
+	for adj_node in edges[node]:
+		# print(f"{adj_node=}")
+		if visited[adj_node] == False:
+			dfs(adj_node)
+
+n, m = map(int, input().split())
+edges = defaultdict(list)
+for _ in range(m):
+	u, v = map(int, input().split())
+	edges[u].append(v)
+	edges[v].append(u)
+
+visited = [ False for _ in range(n+1)]
+# visited[0] = True
+cnt = 0
+for node in range(1, n+1):
+	if visited[node] == False:
+		dfs(node)
+		cnt += 1
+print(cnt)
 
 
+
+'''
+#1. way, union-find
 def find_parent(x):
 	if parent[x] != x:
 		parent[x] = find_parent(parent[x])
@@ -33,18 +69,18 @@ for a_node, b_node in edges:
 	# print(f"{b_parent=}")
 	if a_parent != b_parent:
 		union_parent(a_node, b_node)
+	# print(f"{parent=}")
+
+for node_idx in range(1, n + 1):
+	parent[node_idx] = find_parent(node_idx)
+# print(f"after loop {parent=}")
 
 parent = list(set(parent))
+# indexing의 문제로 0번부터 시작해서 1개를 빼서 return 시킨다.
 print(len(parent)-1)
 # print(f"{parent=}")
 
-
-
 '''
-0 1 2 3
-5 4 7 6
-'''
-
 
 
 
