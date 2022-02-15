0b8791cf5d4376c1a0d5469f0adc3bda2f978302
Jake1152 <rancho974@gmail.com>
2022-02-15 19:18:51 +0900

    done

--- BOJ/matrix_square_10830.py
+++ BOJ/matrix_square_10830.py
@@ -2,31 +2,55 @@
 from sys import stdin
 input = stdin.readline
 
-
-def matrix_square(a_matrix, b_matrix):
+def multiply_matrix(a_matrix, b_matrix):
+	return_matrix = [[0 for j in range(N)] for i in range(N)]
 	for i in range(N):
 		for j in range(N):
 			element_calculate_result = 0
 			for k in range(N):
 				element_calculate_result += (a_matrix[i][k] * b_matrix[k][j])
-			result_matrix[i][j] = element_calculate_result % 1000
-	return result_matrix
+			return_matrix[i][j] = element_calculate_result % 1000
+	return return_matrix
 
+def matrix_square(matrix):
+	return_matrix = [[0 for j in range(N)] for i in range(N)]
+	for i in range(N):
+		for j in range(N):
+			element_calculate_result = 0
+			for k in range(N):
+				element_calculate_result += (matrix[i][k] * matrix[k][j])
+			return_matrix[i][j] = element_calculate_result % 1000
+	return return_matrix
 
-def matrix_square_recursion(a_matrix, b_matrix, power):
-	if (power < 2):
-		return matrix_square(a_matrix, b_matrix)
+def matrix_moduler(base_matrix):
+	for i in range(N):
+		for j in range(N):
+			base_matrix[i][j] = base_matrix[i][j] % 1000
+	return base_matrix
 
-		
-	pass
+def matrix_square_recursion(base_matrix, power):
+	if (power == 1):
+		return matrix_moduler(base_matrix)
+	current_square_matrix = matrix_square_recursion(base_matrix, power//2)
+	if power % 2 == 0:
+		return matrix_square(current_square_matrix)
+	return multiply_matrix(matrix_square(current_square_matrix), org_matrix)
 
-
-N, B = map(int, input().split())
+N, power_cnt = map(int, input().split())
 matrix = []
+org_matrix = []
 for _ in range(N):
 	matrix.append(list(map(int, input().split())))
-result_matrix = [[ matrix[i][j] for j in range(N)] for i in range(N)]
-multiple_matrix = [[ matrix[i][j] for j in range(N)] for i in range(N)]
+for row_vector in matrix:
+	org_matrix.append(row_vector)
+
+result_matrix = matrix_square_recursion(matrix, power_cnt)
+for i in range(N):
+	for j in range(N):
+		print(result_matrix[i][j],  end=' ')
+	print()
+'''
+
 # 행렬을  B번 제곱한다.
 for times in range(2, B+1):
 	# 행렬 곱
@@ -41,3 +65,16 @@ for i in range(N):
 	for j in range(N):
 		print(result_matrix[i][j],  end=' ')
 	print()
+
+
+2 1
+1000 1000
+1000 1000
+0 0
+0 0
+
+3 999
+1 2 3
+4 5 6
+7 8 9
+'''
