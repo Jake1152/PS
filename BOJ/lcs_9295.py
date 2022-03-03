11abeb049b81c6d318aff2be9a22c2a9647e18ec
Jake1152 <rancho974@gmail.com>
2022-03-04 01:59:21 +0900

    lcs 9295

--- BOJ/lcs_9295.py
+++ BOJ/lcs_9295.py
@@ -1,28 +1,53 @@
-A_MAX = 1000
-B_MAX = 1000
+# lcs
+import sys
+sys.setrecursionlimit(10**4)
+STR_MAX = 1001
 
-lcs_table = [ [-1 for _ in range(A_MAX)] for _ in range(B_MAX)]
+lcs_table = [[0 for _ in range(STR_MAX)]for _ in range(STR_MAX)]
 
-def lcs_length(x, y, m, n):
-	#종료 조건은 두 문자열 중 하나가 빈 문자열 일떄
-	#LCS length는 0
-	if (m == 0 or n == 0):
-		return (0)
-	# 이미 계산된 값이 있다면 캐시 값 반환
-	if (lcs_table[m][n] != -1):
-		return (lcs_table[m][n])
+def lcs_len(a_str, b_str, a_idx, b_idx):
+	if a_idx == 0 or b_idx == 0:
+		return 0
+	if lcs_table[a_idx][b_idx] != 0: 
+		return lcs_table[a_idx][b_idx]
 	
-	# 문자열의 마지막 글자를 비교 조건에 따라 재귀 호출
-	if (x[m-1] == y[n-1]):
-		lcs_table[m][n] = 1 + lcs_length(x, y, m - 1, n - 1)
+	if a_str[a_idx-1] == b_str[b_idx-1]:
+		lcs_table[a_idx][b_idx] = 1 + lcs_len(a_str, b_str, a_idx-1, b_idx-1)
 	else:
-		lcs_table[m][n] = max(lcs_length(x, y, m, n -1), lcs_length(x, y, m -1, n))
-	return lcs_table[m][n]
-
+		lcs_table[a_idx][b_idx] = max(lcs_len(a_str, b_str, a_idx, b_idx-1),
+										lcs_len(a_str, b_str, a_idx-1, b_idx))
+	return lcs_table[a_idx][b_idx]
+	
 x = input()
 y = input()
 
-print(lcs_length(x, y, len(x), len(y)))
+print(lcs_len(x, y, len(x), len(y)))
+
+# A_MAX = 1000
+# B_MAX = 1000
+
+# lcs_table = [ [-1 for _ in range(A_MAX)] for _ in range(B_MAX)]
+
+# def lcs_length(x, y, m, n):
+# 	#종료 조건은 두 문자열 중 하나가 빈 문자열 일떄
+# 	#LCS length는 0
+# 	if (m == 0 or n == 0):
+# 		return (0)
+# 	# 이미 계산된 값이 있다면 캐시 값 반환
+# 	if (lcs_table[m][n] != -1):
+# 		return (lcs_table[m][n])
+	
+# 	# 문자열의 마지막 글자를 비교 조건에 따라 재귀 호출
+# 	if (x[m-1] == y[n-1]):
+# 		lcs_table[m][n] = 1 + lcs_length(x, y, m - 1, n - 1)
+# 	else:
+# 		lcs_table[m][n] = max(lcs_length(x, y, m, n -1), lcs_length(x, y, m -1, n))
+# 	return lcs_table[m][n]
+
+# x = input()
+# y = input()
+
+# print(lcs_length(x, y, len(x), len(y)))
 
 
 '''
