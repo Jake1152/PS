0a79ee967cee587655c42e1af5138d7f1b129aa2
Jake1152 <rancho974@gmail.com>
2022-03-21 22:48:10 +0900

    -ing bin search part

--- BOJ/slice_string_2866.py
+++ BOJ/slice_string_2866.py
@@ -1,19 +1,27 @@
 # slice string 2866
 string_cnt, string_len = map(int ,input().split())
 string_matrix = []
-column_str_list = []
+org_column_str_list = []
 for _ in range(string_cnt):
 	string_matrix.append(list(str(input())))
 for i in range(string_len):
 	col_str = []
 	for j in range(string_cnt):
 		col_str += string_matrix[j][i]
-	column_str_list.append(''.join(col_str))
+	org_column_str_list.append(''.join(col_str))
 
 start = 0
 end = string_len
-
+print(f"{org_column_str_list=}")
 while(start <= end):
 	mid = (start + end)//2
-	pass
+	cur_str_set = set()
+	cur_column_str_list = []
+	for idx in range(string_len):
+		cur_column_str_list.append(org_column_str_list[idx][mid:])
+		cur_str_set.add(org_column_str_list[idx][mid:])
+		print(f"{cur_column_str_list=}")
+		print(f"{cur_str_set=}")
+	break
+
 
