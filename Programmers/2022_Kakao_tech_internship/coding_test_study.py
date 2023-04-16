c322126d50cc82fef0f6acd4f9a3147cbde8e16a
Jake1152 <rancho974@gmail.com>
2023-04-16 22:45:49 +0900

    ADD, ING
    정렬 새롭게 해야함
    현재는 알고력만 보지만
    알고력, 코테력 둘다 봐야하며, 둘의 편차도 커서는 안됨
    오름차순으로 알고력, 코테력 둘다 총합으로 작은것과 동등하게 작다면 알고력, 코딩력 작은 순으로 오름차순 정렬
    
    이후에는 연습으로 실력을 키우고 풀수있게 된 문제는 solved_list에 추가한다.
    다음 문제를 풀때 연습으로 실력 키울 수 있는 것 리스트에 올린다.

---
+++ Programmers/2022_Kakao_tech_internship/coding_test_study.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def is_can_be_solved(ability, problem):
+    cur_alp = ability[0]
+    cur_cop = ability[1]
+    problem_alp = problem[0]
+    problem_cop = problem[1]
+    if (cur_alp < problem_alp) or (cur_cop < problem_cop):
+        return (False)
+    return (True)
+
+'''
+최단경로로 가장 빠르게 연습할 수 있는 방법을 찾는다.
+
+소요된 시간을 return한다.
+'''
+def practice(problem, solved_list):
+    if (solved_list):
+        pass
+    pass
+
+def solution(alp, cop, problems):
+    answer = 0
+    solved_list = []
+    asc_problems = deque(sorted(problems, key=lambda x: x[0], x[1]))
+    for problem in asc_problems:
+        # print(f"{problem=}")
+        if is_can_be_solved((alp, cop), problem) == False:
+            answer += practice(problem)
+            
+    return answer
