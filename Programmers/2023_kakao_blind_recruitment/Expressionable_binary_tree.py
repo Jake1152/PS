56274db2b0affe2c6aa38e7056b5144d55cdb95a
Jake1152 <rancho974@gmail.com>
2023-08-20 20:20:23 +0900

    [ADD] 이진트리로 표현가능한 숫자

---
+++ Programmers/2023_kakao_blind_recruitment/Expressionable_binary_tree.py
@@ -0,0 +1,8 @@
+x = input()
+decimal_number = int(x)
+binary_representation = bin(decimal_number)[0:]
+'''
+10 ** 15보다 작은 범위에서 찾을수있는가>
+'''
+btree_node_size_list = []
+print(f"{binary_representation=}")
