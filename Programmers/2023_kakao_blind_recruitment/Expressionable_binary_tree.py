3e56bc8952eabd26c19d38d8a4b207677b670870
Jake1152 <rancho974@gmail.com>
2023-08-27 23:32:16 +0900

    [WIP] add dumy zero for full binary tree style binary number

--- Programmers/2023_kakao_blind_recruitment/Expressionable_binary_tree.py
+++ Programmers/2023_kakao_blind_recruitment/Expressionable_binary_tree.py
@@ -14,21 +14,21 @@ def solution(numbers):
         full_binary_tree_node_count = 2 ** power - 1
         full_binary_tree_node_count_list.append(full_binary_tree_node_count)
         power += 1
-    print(f"{full_binary_tree_node_count_list=}")
-    print(f"# {len(full_binary_tree_node_count_list)=}")
+    # print(f"{full_binary_tree_node_count_list=}")
+    # print(f"# {len(full_binary_tree_node_count_list)=}")
 
     for number in numbers:
         binary_representation = bin(number)[2:]
-        # print(f"{binary_representation=}")
-        # print(f"# {len(binary_representation)=}")
+        print(f"{binary_representation=}")
+        print(f"# {len(binary_representation)=}")
         current_binary_representation_len = len(binary_representation)
         # 같거나 작기 시작할때 알맞은 크기를 구하고서 break
         full_binary_tree_size = [current_binary_representation_len][0]
         '''
-        - 이진수의 길이와 알맞은 포화이진트리 크기를 찾는다
-        - 이진수 길이가 더 짧으면 알맞은 포화이진트리 크기만큼 0을 이진수 앞에 추가해준다.
-        - 이진트리 생성
-        - 이진트리에 넣으면서 확인
+        [v] - 이진수의 길이와 알맞은 포화이진트리 크기를 찾는다
+        [v] - 이진수 길이가 더 짧으면 알맞은 포화이진트리 크기만큼 0을 이진수 앞에 추가해준다.
+        [] - 이진트리 생성
+        [] - 이진트리에 넣으면서 확인
         '''
         for full_binary_tree_node_count in full_binary_tree_node_count_list:
             if current_binary_representation_len <= full_binary_tree_node_count:
@@ -37,11 +37,12 @@ def solution(numbers):
         # print(f"{full_binary_tree_size=}")
         if (full_binary_tree_size > current_binary_representation_len):
             pass
-        zero_list= []
         zero_list = [ "0" for _ in range(full_binary_tree_size - current_binary_representation_len)]
-        # print(f"{zero_list=}")
+        binary_string = ''.join([''.join(zero_list), binary_representation])
+        print(f"## {binary_string=}")
+        # ''.join([''.join(zero_list), binary_representation])
         # "0101010"
-        ''.join([(''.join(zero_list)), current_binary_representation_len])
+        # ''.join([(''.join(zero_list)), current_binary_representation_len])
         # current_binary_string = ''.join([(''.join(zero_list)), current_binary_representation_len])
         # print(f"{current_binary_string=}")
             
@@ -49,7 +50,8 @@ def solution(numbers):
 
     return answer
 
-numbers = [7,42,5]
+# numbers = [7,42,5]
+numbers = [63, 1212323, 94]
 print(f'#{solution(numbers)=}')
 # [1,1,0]
 '''
@@ -57,4 +59,5 @@ print(f'#{solution(numbers)=}')
 '''
 # btree_node_size_list = []
 # print(f"{binary_representation=}")
+
 
