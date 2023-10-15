31d417e755402d29e74a5e9017aee6e3bb389f1f
Jake1152 <rancho974@gmail.com>
2023-10-16 02:40:00 +0900

    [WIP] dfs, find any other sheep in sub tree

--- Programmers/2022_kakao_blind_recruitment/sheep_and_wolf.py
+++ Programmers/2022_kakao_blind_recruitment/sheep_and_wolf.py
@@ -15,8 +15,20 @@ def can_I_go_there(cur_node, info, animal_farm):
         return False
     return True
 
-def is_there_any_other_sheep(cur_node, info, edges):
-    pass
+def dfs(root_node, tree, info, acc):
+    if info[root_node] == 0:
+        acc += 1
+    for cur_node in tree[root_node]['child']:
+        acc = dfs(cur_node, tree, info, acc)
+    return acc
+'''
+단지 서브 트리에 양이 있는지만 확인한다
+몇마리인지, 거기까지 갈만한지는 확인하지 않는다.
+'''
+def is_there_any_other_sheep(cur_node, tree, info):
+    if dfs(cur_node, tree, info, 0) > 0:
+        return True
+    return False
 
 '''
 tree = {node{parent: int , child: [int, int]}, ..., node{parent: int , child: [int, int]}}
@@ -40,18 +52,20 @@ def solution(info, edges):
     visited = [False for _ in range(len(info))]
     tree = make_tree(sorted_edges)
     print(f"{tree=}")
-
+    print(f"{is_there_any_other_sheep(0, tree, info)=}")
 
     animal_farm = {'sheep': 0, 'wolf': 0}
     for cur_node in tree:
         can_I_go_there(cur_node, info, animal_farm)
+
     # print(f"{animal_farm=}")
+
     return animal_farm['sheep']
 
-# info = [0,0,1,1,1,0,1,0,1,0,1,1]
-# edges = [[0,1],[1,2],[1,4],[0,8],[8,7],[9,10],[9,11],[4,3],[6,5],[4,6],[8,9]]
+info = [0,0,1,1,1,0,1,0,1,0,1,1]
+edges = [[0,1],[1,2],[1,4],[0,8],[8,7],[9,10],[9,11],[4,3],[6,5],[4,6],[8,9]]
 
-info = [0,1,0,1,1,0,1,0,0,1,0]
-edges = [[0,1],[0,2],[1,3],[1,4],[2,5],[2,6],[3,7],[4,8],[6,9],[9,10]]
+# info = [0,1,0,1,1,0,1,0,0,1,0]
+# edges = [[0,1],[0,2],[1,3],[1,4],[2,5],[2,6],[3,7],[4,8],[6,9],[9,10]]
 
 print(f"{solution(info, edges)=}")
