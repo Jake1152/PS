47496b2feab5fb31bffad02d51bbf979e7367637
Jake1152 <rancho974@gmail.com>
2023-11-05 22:06:59 +0900

    [FIX] 2번째 시도, arr없이 k번째 약수 찾아서 return
    - 반복문 안에서 k가 0이 되는지를 매 번 확인하는데 매 번 조건문 확인하지 않고 처리할 수 있는 방법 있는지 생각해본다.

--- CodeTestLecture/section_02/practice/01_kth_divisor.py
+++ CodeTestLecture/section_02/practice/01_kth_divisor.py
@@ -1,9 +1,16 @@
 N, K = map(int, input().split())
-divisor_arr = []
+# divisor_arr = []
+cnt = 0
 for divisor in range(1, N+1):
     if (N % divisor == 0):
-        divisor_arr.append(divisor)
-if (len(divisor_arr) + 1> K ):
-    print(divisor_arr[K-1])
+        K -= 1
+    if K == 0:
+        print(divisor)
+        break
 else:
     print(-1)
+    
+# if (len(divisor_arr) + 1> K ):
+#     print(divisor_arr[K-1])
+# else:
+#     print(-1)
