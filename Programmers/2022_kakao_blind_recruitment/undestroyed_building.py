8ebc4fd5acd83e67bf0380cd710a533af81556c3
Jake1152 <rancho974@gmail.com>
2023-12-24 20:58:25 +0900

    [ADD] Done, 누적합, 알면 쉽지만 모를때는 감이 안온다
    - DP라고만 생각했는데 누적합이었다.
    - 2D인 경우의 응용을 못한게 이유였다.
    - 어떻게 응용할 것인가?
      - O(n)으로 하면 오래걸리지만 구간이 있는 것
      - 일일이 매번 연산하는 것이 아니라 한번에 연산할 수 있게 구조를 만든다.

--- Programmers/2022_kakao_blind_recruitment/undestroyed_building.py
+++ Programmers/2022_kakao_blind_recruitment/undestroyed_building.py
@@ -1,14 +1,58 @@
 def solution(board, skills):
     answer = 0
+    # 스킬 수만큼 반복
+    # 25만 * 100만 => 2500억
+    dp = [[0] * (len(board[0]) + 1) for _ in range(len(board) + 1)]
+    '''
+    0~n까지만 하는게 아니라 n+1지점에 상쇄할 수 있는 값을 넣는 이유
+    전체영역에 누적합부분을 한번에 더하는 연산으로 처리할 수 있게하기 위함 
+    0~2까지 영역에 N을 더하는 것 이라면 아래와 같이 4개의 꼭지점을 표현한다 
+    
+    0. 누적 초기
+    [ [N, 0, 0, -N, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [-N, 0, 0, N, 0] ]
+    => 1. 행 우선 누적
+    [ [N, N, N, 0, 0],
+      [0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0],
+      [-N, -N, -N, 0, 0] ]
+    => 2. 열 우선 누적
+    [ [N, N, N, 0, 0],
+      [N, N, N, 0, 0],
+      [N, N, N, 0, 0],
+      [0, 0, 0, 0, 0] ]
+    => 3. 전체 영역 누적 
+
+    '''
     for skill in skills:
         skill_type, r1, c1, r2, c2, degree = skill
-        degree = -degree if skill_type == 1 else degree
-        for x in range(r1, r2 + 1):
-            for y in range(c1, c2 + 1):
-                board[x][y] += degree
-    for rows in board:
-        for element in rows:
-            if element > 0:
+        dp[r1][c1] += degree if skill_type == 2 else -degree
+        dp[r1][c2+1] += -degree if skill_type == 2 else degree
+        dp[r2 + 1][c1] += -degree if skill_type == 2 else degree
+        dp[r2 + 1][c2 + 1] += degree if skill_type == 2 else -degree
+        print(f"#{dp=}")
+
+    # 맵에 있는 건물들 순회하며 degree 더하기
+    #  행 기준 누적합
+    for x in range(len(dp) - 1):
+        for y in range(len(dp[0])-1):
+            dp[x][y + 1] += dp[x][y]
+    #  열 기준 누적합
+    for y in range(len(dp[0]) - 1):
+        for x in range(len(dp) - 1):
+            dp[x + 1][y] += dp[x][y]
+    # 기존 배열과 누적 배열의 합 and 살아남은 건물 카운팅
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            board[x][y] += dp[x][y]
+            if board[x][y] > 0:
                 answer += 1
     return answer
 
+
+
+# board = [[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5],[5,5,5,5,5]]
+# skill= [[1,0,0,3,4,4],[1,2,0,2,3,2],[2,1,0,3,1,2],[1,0,1,3,3,1]]
+# print(f"{solution(board, skill)=}")
