4c2ea9dcf75dcd03ddb8de7e74f636154aefd8ea
Jake1152 <rancho974@gmail.com>
2024-01-21 21:19:14 +0900

    [ADD] 가장 많이 받은 선물, 레벨 1, 자료구조에 담아서 처리
    - dict를 이용한 처리
      문제지문을 놓친 부분이 있어서 디버깅 끝에 해결
      - 선물을 서로 주고 받지 않은 경우이더라도 선물지수가 더 높은 사람한테 다음달에 선물을 하나 준다

---
+++ Programmers/2024_KAKAO_WINTER_INTERNSHIP/most_received_gifts.py
@@ -0,0 +1,45 @@
+'''
+# 사람별로 다른 사람에서 선물을 준 갯수, 받은 갯수, 선물지수(준것cnt - 받은것cnt)를 기록
+# 갯수가 같은 경우에 선물지수 비교
+'''
+from collections import defaultdict
+def solution(friends, gifts):
+    # gift_score
+    answer = 0
+    # 자료구조 생성 
+    account_dict = {}
+    for friend in friends:
+        account_dict[friend] = {'gift_score': 0, 'assume': 0, 'account_book': defaultdict(int)}
+        for my_friend in friends:
+            if my_friend != friend:
+                account_dict[friend]['account_book'][my_friend] = 0
+    # print(f"{account_dict=}")
+    # 기록 누적 record
+    for gift in gifts:
+        sender, receiver = gift.split()
+        # value['gift_score']
+        account_dict[sender]['gift_score'] += 1
+        account_dict[sender]['account_book'][receiver] += 1
+        account_dict[receiver]['gift_score'] -= 1
+    # 받을 선물 추정치 계산, 동률인 경우 포함
+    '''
+    나라는 사람과 내가 선물 준 다른 친구들과의 선물 주고 받은 갯수의 차이를 계산
+    '''
+    for key, value in account_dict.items():
+        # print(f"{key}: {account_dict[key]}")
+        # print(f"{key}: {value}")
+        
+        # account_dict
+        for my_friend_name, my_sent_cnt in value['account_book'].items():
+            # 내가 받은 갯수
+            my_received_cnt = account_dict[my_friend_name]['account_book'][key]
+            # print(f"## my={key} {my_friend_name=} {my_sent_cnt=} {my_received_cnt=} { account_dict[my_friend_name]['gift_score']=}")
+            if my_sent_cnt > my_received_cnt:
+                account_dict[key]['assume'] += 1
+            elif my_sent_cnt == my_received_cnt and account_dict[key]['gift_score'] > account_dict[my_friend_name]['gift_score']:
+                account_dict[key]['assume'] += 1
+    # print(f"{account_dict=}")
+    for _dict in account_dict.values():
+        answer = max(answer, _dict['assume'])
+    return answer
+
