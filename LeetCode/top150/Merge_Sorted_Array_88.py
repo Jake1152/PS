69a2021a8e594e7f63911c09e1152d64eb41c381
Jake1152 <rancho974@gmail.com>
2024-06-09 19:28:05 +0900

    Feat: LeetCode 88. Merge Sorted Array
    - first try

---
+++ LeetCode/top150/Merge_Sorted_Array_88.py
@@ -0,0 +1,58 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        result = []
+
+        idx_i = 0
+        idx_j = 0
+        nums1_len = len(nums1)
+        nums2_len = len(nums2)
+        copy_nums1 = [element for element in nums1]
+        
+        idx = 0
+        min_nums1 = 0
+        if (nums1):
+            min_nums1 = nums1[0]
+        min_nums2 = 0
+        if (nums2):
+            min_nums2 = nums2[0]
+        prev_value = min(min_nums1, min_nums2)
+        while (idx < m + n and idx_i < m and idx_j < n):
+
+            if (prev_value > copy_nums1[idx_i]):
+                idx_i += 1
+                continue
+            
+            if (prev_value > nums2[idx_j]):
+                idx_j += 1
+                continue
+            
+            if (copy_nums1[idx_i] < nums2[idx_j]):
+                nums1[idx] = copy_nums1[idx_i]
+                prev_value = nums1[idx]
+                idx_i += 1
+                idx += 1
+            # else: #if (nums1[idx_i] >= nums2[idx_j]):
+            elif (copy_nums1[idx_i] >= nums2[idx_j] and prev_value <= nums2[idx_j]):
+                nums1[idx] = nums2[idx_j]
+                prev_value = nums1[idx]
+                idx_j += 1
+                idx += 1
+        while (idx < n + m and idx_i < m):
+            nums1[idx] = copy_nums1[idx_i]
+            prev_value = nums1[idx]
+            idx_i += 1
+            idx += 1
+        
+        while (idx < n + m and idx_j < n):
+            nums1[idx] = nums2[idx_j]
+            prev_value = nums1[idx]
+            idx_j += 1
+            idx += 1
+        
+   
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+    
+
+    
