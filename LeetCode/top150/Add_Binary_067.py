8ad7bbcff955ea233701f654cbf98f273b1629b3
jake <rancho974@gmail.com>
2025-12-07 18:55:28 +0900

    feat: [LeetCode] 67. Add Binary

--- LeetCode/top150/Add_Binary_067.py
+++ LeetCode/top150/Add_Binary_067.py
@@ -1,11 +1,32 @@
 from collections import deque
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        # Python 라이브러리 활용
+        # print(f"int(a): {int(a)}")
+        # print(f"bin(a) : {bin(a)}")
+        min_len_of_two_str  = min(len(a), len(b))
+        result_dec = 0
+        for digit in range(len(a)):
+            idx = len(a)-1 - digit
+            a_num = int(a[idx])
+            result_dec += a_num * 2 ** digit
+
+        for digit in range(len(b)):
+            idx = len(b)-1 - digit
+            b_num = int(b[idx])
+            result_dec += b_num * 2 ** digit
+        result = str(bin(result_dec))
+        return result[2:]
+        # result_bin = bin(int(a)) + bin(int(b))
+        # print(result_bin)
+        # return str(result_bin)
+        # print(f"result_bin: {result_bin}")
         # Brute force version
         '''
         1.  문자열을 a,b를 1의 자리부터 더해서 result를 만든다.
           - carray 변수 사용 
         2. result 문자열을 이진수로 계산한다.
+        '''
         '''
         result_bin = deque()
 
@@ -59,3 +80,4 @@ class Solution:
         if carry == 1:
             result_bin.appendleft("1")
         return str("".join(result_bin))
+        '''
